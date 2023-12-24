87cb807fabcc36c24a389078330e299ccff74b05
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2023-12-24 19:31:17 +0100

    Style fixed

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -5,7 +5,7 @@ integer arguments page and page_size
 """
 
 
-def index_range(page: int, page_size:int):
+def index_range(page: int, page_size: int):
     """
     a function named index_range that takes two
     integer arguments page and page_size.
