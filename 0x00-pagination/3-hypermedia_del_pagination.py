9b774488897f7c8f5b312b87e414f2a6de4c4613
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2023-12-25 17:55:14 +0100

    Added the comment to the get_hyper_index function

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -40,7 +40,7 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-        """"""
+        """Get hyper index"""
         assert index < len(self.dataset()) and index >= 0
 
         data = []
