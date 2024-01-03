57f9163e1d7ea96b51afc1120f5ae65efcc8801e
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2024-01-03 16:25:08 +0100

    Added 1-fifo_cache.py

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+
+"""
+a class Fifo cache that inherits from BaseCaching and is a caching system
+"""
+
+
+from base_caching import BaseCaching
+
+class FIFOCache(BaseCaching):
+    """A Cache that acts in the First in
+    first out manner
+    """
+
+    def put(self, key, item):
+        """Stores data into the cache using the FIFO
+        manner
+        """
+        if key is None or item is None:
+            return
+        if len(self.cache_data) == self.MAX_ITEMS:
+            keys = list(self.cache_data.keys())
+            print(f"DISCARD: {keys[0]}")
+            self.cache_data.pop(keys[0])
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """retrieve data stored in the cache"""
+        if key is None:
+            return None
+        return self.cache_data.get(key, None)
