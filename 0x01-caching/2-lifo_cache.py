ca44dd6ccd0c70fb9e796fb9dab5f78372c5b494
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2024-01-03 16:55:40 +0100

    Added 2-lifo_cache.py and made corrections to 1-fifo_cahe.py

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -2,28 +2,28 @@
 
 
 """
-a class Fifo cache that inherits from BaseCaching and is a caching system
+a class Lifo cache that inherits from BaseCaching and is a caching system
 """
 
 
 from base_caching import BaseCaching
 
-class FIFOCache(BaseCaching):
-    """A Cache that acts in the First in
+class LIFOCache(BaseCaching):
+    """A Cache that acts in the Last in
     first out manner
     """
 
     def put(self, key, item):
-        """Stores data into the cache using the FIFO
+        """Stores data into the cache using the LIFO
         manner
         """
         if key is None or item is None:
             return
         keys = list(self.cache_data.keys())
-        if len(self.cache_data) == self.MAX_ITEMS and key not in keys:
-            keys = list(self.cache_data.keys())
-            print(f"DISCARD: {keys[0]}")
-            self.cache_data.pop(keys[0])
+        if len(self.cache_data) >= self.MAX_ITEMS and key not in keys:
+            last_key = keys[len(keys) - 1]
+            print(f"DISCARD: {last_key}")
+            self.cache_data.pop(last_key)
         self.cache_data[key] = item
 
     def get(self, key):
