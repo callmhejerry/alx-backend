bbd772f5264ca000e1eac0d16d5ad1d06978ef23
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2024-01-16 01:21:09 +0100

    Added 3-app.py

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+Babel setup
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+
+class Config(object):
+    """Babel Configuration"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__, template_folder='templates')
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Locale language
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """Greetings
+    """
+    return render_template('3-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
