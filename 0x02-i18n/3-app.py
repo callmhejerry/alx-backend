7b90feb2d90542dd49cd7a305d85d010aadcce79
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2024-01-16 02:18:12 +0100

    hot fix

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 """
-Babel setup
+Simple flask app
 """
 from flask import Flask, render_template, request
 from flask_babel import Babel, _
 
 
 class Config(object):
-    """Babel Configuration"""
+    """Babel Configuration class"""
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
@@ -21,6 +21,8 @@ babel = Babel(app)
 @babel.localeselector
 def get_locale():
     """Locale language
+    returns:
+        description
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
